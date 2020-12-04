e05a8096d15fb524f67f14c9833273a7d00ded01
Geoff Macartney <geoff.macartney@ibm.com>
2020-12-04 13:50:07 +0000

    handle multiple files

--- exc_test.go
+++ exc_test.go
@@ -3,10 +3,12 @@ package main
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
-func TestName(t *testing.T) {
-	result, err := process("data.txt")
+func TestProcess(t *testing.T) {
+	counts := make(map[string]int, 0)
+	err := process("data.txt", counts)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -16,7 +18,25 @@ func TestName(t *testing.T) {
 		"and":       1,
 		"curiouser": 2,
 	}
-	if !reflect.DeepEqual(result, expected) {
-		t.Fatalf("expected %v but got %v", expected, result)
+	if !reflect.DeepEqual(counts, expected) {
+		t.Fatalf("expected %v but got %v", expected, counts)
+	}
+}
+
+func TestProcessAll(t *testing.T) {
+	counts := make(map[string]int, 0)
+	err := processAll([]string{"data.txt", "data.txt"}, counts)
+	if err != nil {
+		t.Fatal(err)
+	}
+	time.Sleep(2 * time.Second)
+	expected := map[string]int{
+		"alice":     2,
+		"cried":     2,
+		"and":       2,
+		"curiouser": 4,
+	}
+	if !reflect.DeepEqual(counts, expected) {
+		t.Fatalf("expected %v but got %v", expected, counts)
 	}
 }
